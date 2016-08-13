58f0d1b094cea617ce228d5768d1c864aa2d00d0
S <slv.balsan@gmail.com>
2016-08-13 14:45:26 +0200

    working fetch

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ import (
 	"time"
 )
 
+var debugging = true
+
 var (
 	username string
 	password string
@@ -30,20 +32,27 @@ var (
 
 var (
 	res          Resumer
-	outputWriter io.WriteCloser
+	outputWriter *bufio.Writer
 
 	resumerSuffix string = ".audisto_"
 )
 
 type Resumer struct {
 	OutputFilename string
-	OutputPath     string // path minus filename
 
 	DoneElements  int64
 	TotalElements int64
-	ChunkSize     int64
+	chunkSize     int64
 
 	httpClient http.Client
+}
+
+type chunk struct {
+	Chunk struct {
+		Total int64
+		Page  int
+		Size  int
+	}
 }
 
 func init() {
@@ -75,86 +84,89 @@ func init() {
 
 	// stdout or output file ?
 	if output == "" {
-		outputWriter = os.Stdout
+		outputWriter = bufio.NewWriter(os.Stdout)
 	} else {
 		// If don't resume, create new set
 		if noResume {
+
+			// if resume, check if output file exists
+			if err := fExists(output); err == nil {
+				panic("File already exists; please delete or specify another output filename")
+			}
+
 			var err error
 
-			newConfig, err := json.Marshal(Resumer{})
+			res = Resumer{}
+
+			res.TotalElements, err = totalElements()
 			if err != nil {
 				panic(err)
 			}
+			res.OutputFilename = output
 
-			// create {{output}}.audisto_ file (keeps track of progress etc.)
-			err = ioutil.WriteFile(output+resumerSuffix, newConfig, 0644)
+			err = res.PersistConfig()
 			if err != nil {
 				panic(err)
 			}
 
 			// create new outputFile
-			outputWriter, err = os.Create(output)
+			newFile, err := os.Create(output)
 			if err != nil {
 				panic(err)
 			}
-		}
+			outputWriter = bufio.NewWriter(newFile)
+		} else {
+			// if resume, check if output file exists
+			if err := fExists(output); err != nil {
+				panic(fmt.Sprint("Cannot resume; output file does not exist: ", err))
+			}
+			// if resume, check if resume file exists
+			if err := fExists(output + resumerSuffix); err != nil {
+				panic(fmt.Sprint("Cannot resume; resume file does not exist: ", err))
+			}
 
-		// if resume, check if output file exists
-		if err := fExists(output); err != nil {
-			panic(fmt.Sprint("Cannot resume; output file does not exist: ", err))
-		}
-		// if resume, check if resume file exists
-		if err := fExists(output + resumerSuffix); err != nil {
-			panic(fmt.Sprint("Cannot resume; resume file does not exist: ", err))
-		}
+			resumerFile, err := ioutil.ReadFile(output + resumerSuffix)
+			if err != nil {
+				panic(fmt.Sprintf("File error: %v\n", err))
+			}
+			json.Unmarshal(resumerFile, &res)
 
-		// read and validate resumer file
-		// read and validate output file
+			// open outputFile
+			existingFile, err := os.OpenFile(output, os.O_WRONLY|os.O_APPEND, 0777)
+			if err != nil {
+				panic(err)
+			}
+			outputWriter = bufio.NewWriter(existingFile)
+
+			// read and validate resumer file
+			// read and validate output file
+
+		}
 
 	}
 
-}
+	res.chunkSize = 10000
 
-// fExists returns nil if path is an existing file/folder
-func fExists(path string) error {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return err
-	}
-	return nil
-}
-
-func IsDirectory(path string) (bool, error) {
-	fileInfo, err := os.Stat(path)
-	return fileInfo.IsDir(), err
-}
-
-func usage() {
-	fmt.Fprintf(os.Stderr, `usage: audistoDownloader [flags]
-Flags:
-  username    API Username (required)
-  password    API Password (required)
-  crawl       ID of the crawl to download (required)
-  no-details  If passed, details in API request is set to 0 else
-  output      Path for the output file
-  no-resume   If passed, download starts again, else the download is resumed
-`)
 }
 
 func main() {
-	fmt.Println(username, password, crawl)
+	debug(username, password, crawl)
 
 	// outputWriter.Write([]byte("hi there"))
 
-	failCount := 0
 	timeoutCount := 0
 
+	debug("%#v\n", res)
 	for {
-		chunk, statusCode, total, skip, err := res.nextChunk()
+		debug("calling next chunk")
+		chunk, statusCode, skip, err := res.nextChunk()
 		if err != nil {
-			fmt.Println(err)
-			time.Sleep(time.Second * 1)
+			debug("error while calling next chunk; %v\n", err)
+			time.Sleep(time.Second * 5)
 			continue
 		}
+		debug("next chunk obtained")
+		debug("statusCode: %v", statusCode)
 
 		// check status code
 		switch {
@@ -176,7 +188,7 @@ func main() {
 						fmt.Println("Not found. Correct crawl ID?")
 						return
 					}
-				case 404:
+				default:
 					{
 						fmt.Println("Unknown error occured")
 						return
@@ -189,8 +201,8 @@ func main() {
 			{
 				timeoutCount += 1
 				if timeoutCount > 3 {
-					if (res.ChunkSize - 10) > 0 {
-						res.ChunkSize = res.ChunkSize - 10
+					if (res.chunkSize - 10) > 0 {
+						res.chunkSize = res.chunkSize - 10
 					}
 				}
 				time.Sleep(time.Second * 30)
@@ -204,22 +216,29 @@ func main() {
 		}
 
 		scanner := bufio.NewScanner(bytes.NewReader(chunk))
+		debug("chunk len: %v", len(chunk))
 
 		// is DoneElements == 0, don't skip first line
 		if res.DoneElements == 0 {
 			scanner.Scan()
 			outputWriter.Write(scanner.Bytes())
+			outputWriter.Write([]byte("\n"))
 		}
 
 		// skip lines
 		for i := int64(0); i < skip; i++ {
 			scanner.Scan()
-			fmt.Println("skipped this row: ", scanner.Text())
+			debug("skipped this row: ", scanner.Text())
 		}
 
 		for scanner.Scan() {
 			outputWriter.Write(scanner.Bytes())
+			outputWriter.Write([]byte("\n"))
+			res.DoneElements += 1
 		}
+
+		outputWriter.Flush()
+		debug("res.DoneElements = %v", res.DoneElements)
 
 		if err := scanner.Err(); err != nil {
 			fmt.Println(err)
@@ -227,6 +246,13 @@ func main() {
 		}
 
 	}
+}
+
+func debug(format string, a ...interface{}) (n int, err error) {
+	if debugging {
+		return fmt.Printf("\n"+format+"\n", a...)
+	}
+	return 0, nil
 }
 
 func (r *Resumer) nextChunkNumber() (nextChunkNumber, skipNRows int64) {
@@ -237,14 +263,14 @@ func (r *Resumer) nextChunkNumber() (nextChunkNumber, skipNRows int64) {
 		return
 	}
 
-	skipNRows = r.DoneElements % r.ChunkSize
-	nextChunkNumberFloat, _ := math.Modf(float64(r.DoneElements) / float64(r.ChunkSize))
+	skipNRows = r.DoneElements % r.chunkSize
+	nextChunkNumberFloat, _ := math.Modf(float64(r.DoneElements) / float64(r.chunkSize))
 
 	nextChunkNumber = int64(nextChunkNumberFloat)
 	return
 }
 
-func (r *Resumer) nextChunk() ([]byte, int, int64, int64, error) {
+func (r *Resumer) nextChunk() ([]byte, int, int64, error) {
 
 	nextChunkNumber, skipNRows := r.nextChunkNumber()
 
@@ -260,24 +286,20 @@ func (r *Resumer) nextChunk() ([]byte, int, int64, int64, error) {
 
 	queryParameters := url.Values{}
 	if noDetails {
+		queryParameters.Add("deep", "0")
+	} else {
 		queryParameters.Add("deep", "1")
 	}
 	queryParameters.Add("chunk", strconv.FormatInt(nextChunkNumber, 10))
-	queryParameters.Add("chunk_size", strconv.FormatInt(r.ChunkSize, 10))
+	queryParameters.Add("chunk_size", strconv.FormatInt(r.chunkSize, 10))
 	queryParameters.Add("output", "tsv")
 
 	body, statusCode, err := r.fetchRawChunk(path, method, headers, queryParameters, bodyParameters)
 	if err != nil {
-		return []byte(""), 0, 0, 0, err
+		return []byte(""), 0, 0, err
 	}
 
-	bodyReader := bytes.NewReader(body)
-	totalLines, err := lineCounter(bodyReader)
-	if err != nil {
-		return []byte(""), 0, 0, 0, err
-	}
-
-	return body, statusCode, int64(totalLines), skipNRows, nil
+	return body, statusCode, skipNRows, nil
 }
 
 func lineCounter(r io.Reader) (int, error) {
@@ -314,6 +336,7 @@ func (r *Resumer) fetchRawChunk(path string, method string, headers http.Header,
 		return []byte(""), 0, fmt.Errorf("Method not supported: %v", method)
 	}
 
+	debug("request url: %s", requestURL.String())
 	request, err := http.NewRequest(method, requestURL.String(), bytes.NewBufferString(bodyParameters.Encode()))
 	if err != nil {
 		return []byte(""), 0, fmt.Errorf("Failed to get the URL %s: %s", requestURL, err)
@@ -351,4 +374,110 @@ func (r *Resumer) fetchRawChunk(path string, method string, headers http.Header,
 	}
 
 	return responseBody, response.StatusCode, nil
+}
+
+func totalElements() (int64, error) {
+	var body []byte
+	var statusCode int
+	err := retry(5, 1, func() error {
+		var err error
+		body, statusCode, err = res.fetchTotalElements()
+		if err != nil {
+			return err
+		}
+		if statusCode != 200 {
+			err = fmt.Errorf("error while trying to get total number of elements; statusCode %v", statusCode)
+		}
+		return err
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	var firstChunk chunk
+	err = json.Unmarshal(body, &firstChunk)
+	if err != nil {
+		return 0, err
+	}
+
+	return firstChunk.Chunk.Total, nil
+}
+
+func (r *Resumer) fetchTotalElements() ([]byte, int, error) {
+
+	path := fmt.Sprintf("/2.0/crawls/%v/pages", crawl)
+	method := "GET"
+
+	headers := http.Header{}
+	bodyParameters := url.Values{}
+
+	queryParameters := url.Values{}
+	queryParameters.Add("deep", "0")
+	queryParameters.Add("chunk", "0")
+	queryParameters.Add("chunk_size", "1")
+	queryParameters.Add("output", "json")
+
+	body, statusCode, err := r.fetchRawChunk(path, method, headers, queryParameters, bodyParameters)
+	if err != nil {
+		return []byte(""), 0, err
+	}
+
+	return body, statusCode, nil
+}
+
+// PersistConfig saves the accounts to file
+func (r *Resumer) PersistConfig() error {
+	config, err := json.MarshalIndent(r, "", "	")
+	if err != nil {
+		return err
+	}
+
+	// create {{output}}.audisto_ file (keeps track of progress etc.)
+	err = ioutil.WriteFile(output+resumerSuffix, config, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func retry(attempts int, sleep int, callback func() error) (err error) {
+	for i := 0; ; i++ {
+		err = callback()
+		if err == nil {
+			return nil
+		}
+
+		if i >= (attempts - 1) {
+			break
+		}
+
+		time.Sleep(time.Duration(sleep) * time.Second)
+		debug("Something failed, retrying;")
+	}
+	return fmt.Errorf("Abandoned after %d attempts, last error: %s", attempts, err)
+}
+
+// fExists returns nil if path is an existing file/folder
+func fExists(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+	return nil
+}
+
+func IsDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	return fileInfo.IsDir(), err
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `usage: audistoDownloader [flags]
+Flags:
+  username    API Username (required)
+  password    API Password (required)
+  crawl       ID of the crawl to download (required)
+  no-details  If passed, details in API request is set to 0 else
+  output      Path for the output file
+  no-resume   If passed, download starts again, else the download is resumed
+`)
 }
