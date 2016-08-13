1b90e7924effe3af92eb9dc2f4684b2b1640ff68
S <slv.balsan@gmail.com>
2016-08-13 09:52:51 +0200

    download anew or resume logic

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -16,6 +19,23 @@ var (
 	output    string
 	noResume  bool
 )
+
+var (
+	outputWriter io.WriteCloser
+
+	resumerSuffix string = ".audisto_"
+)
+
+type Resumer struct {
+	OutputFilename string
+	OutputPath     string // path minus filename
+
+	Crawl     uint64
+	NoDetails bool
+
+	LatestDown string // filename of latest temp file downloaded
+
+}
 
 func init() {
 
@@ -32,8 +52,9 @@ func init() {
 
 	username = strings.TrimSpace(username)
 	password = strings.TrimSpace(password)
+	output = strings.TrimSpace(output)
 
-	// Check for empty flags
+	// Check for non-valid flags
 	usernameIsNull := username == ""
 	passwordIsNull := password == ""
 	crawlIsNull := crawl == 0
@@ -43,6 +64,59 @@ func init() {
 		os.Exit(0)
 	}
 
+	// stdout or output file ?
+	if output == "" {
+		outputWriter = os.Stdout
+	} else {
+		// If don't resume, create new set
+		if noResume {
+			var err error
+
+			newConfig, err := json.Marshal(Resumer{})
+			if err != nil {
+				panic(err)
+			}
+
+			// create {{output}}.audisto_ file (keeps track of progress etc.)
+			err = ioutil.WriteFile(output+resumerSuffix, newConfig, 0644)
+			if err != nil {
+				panic(err)
+			}
+
+			// create new outputFile
+			outputWriter, err = os.Create(output)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		// if resume, check if output file exists
+		if err := fExists(output); err != nil {
+			panic(fmt.Sprint("Cannot resume; output file does not exist: ", err))
+		}
+		// if resume, check if resume file exists
+		if err := fExists(output + resumerSuffix); err != nil {
+			panic(fmt.Sprint("Cannot resume; resume file does not exist: ", err))
+		}
+
+		// read and validate resumer file
+		// read and validate output file
+
+	}
+
+}
+
+// fExists returns nil if path is an existing file/folder
+func fExists(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+	return nil
+}
+
+func IsDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	return fileInfo.IsDir(), err
 }
 
 func usage() {
@@ -59,4 +133,6 @@ Flags:
 
 func main() {
 	fmt.Println(username, password, crawl)
+
+	outputWriter.Write([]byte("hi there"))
 }
